45a38015b60d0830de939858f900d41b8b7590b6
Stefan Moravcik <stefan@virsas.com>
2022-04-25 19:36:35 +0200

    added region

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+  region = var.region
+}
+
 resource "aws_cloudwatch_metric_alarm" "alarm" {
   alarm_name          = var.alarm.name
   alarm_actions       = var.sns
--- variables.tf
+++ variables.tf
@@ -1,3 +1,4 @@
+variable "region" { default = "eu-west-1" }
 variable "filter" {}
 variable "sns" {}
 variable "alarm" { default = {} }
